6025d86c60f75c7d4c58a46ad30081242c599a3d
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-10-13 23:24:17 +0900

    이중우선순위큐

---
+++ 난이도 3/이중우선순위큐.js
@@ -0,0 +1,21 @@
+// 난이도 3, 이중우선순위큐
+
+function solution(operations) {
+  var answer = [];
+
+  operations.map((item) => {
+    var arr = item.split(" ");
+    if (arr[0] === "D" && answer.length > 0) {
+      if (Number(arr[1]) > 0) {
+        answer.splice(answer.indexOf(Math.max(...answer)), 1);
+      } else {
+        answer.splice(answer.indexOf(Math.min(...answer)), 1);
+      }
+    } else if (arr[0] === "I") {
+      answer.push(Number(arr[1]));
+    }
+  });
+  return answer.length > 0
+    ? [Math.max(...answer), Math.min(...answer)]
+    : [0, 0];
+}
