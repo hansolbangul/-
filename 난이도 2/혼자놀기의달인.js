f44cf1d7815c242274d471afa58e83388368962d
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-10-18 00:22:08 +0900

    혼자 놀기의 달인

--- 난이도 2/혼자놀기의달인.js
+++ 난이도 2/혼자놀기의달인.js
@@ -1,28 +1,25 @@
 // 난이도 2, 혼자 놀기의 달인
 
 function solution(cards) {
-  var arr = [];
-  cards.map((_, index) => {
-    var item = _;
-    var temp = [item];
-    for (var i = 0; i < cards.length; ) {
-      if (!temp.some((item) => item === cards[item - 1]))
-        temp.push(cards[item - 1]);
-      if (cards[item - 1] === index + 1) {
-        break;
-      } else {
-        item = cards[item - 1];
+  var answer = [];
+  cards.forEach((value, index) => {
+    let i = value - 1
+    if (value !== 0) {
+      for (let count = 1; count <= cards.length;) {
+        if (cards[i] === value) {
+          cards[i] = 0
+          answer.push(count)
+          break
+        } else {
+          count++
+          const temp = cards[i] - 1
+          cards[i] = 0
+          i = temp
+        }
       }
     }
-    if (
-      !arr.some((item) => JSON.stringify(item) === JSON.stringify(temp.sort()))
-    ) {
-      arr.push(temp.sort());
-    }
-  });
-  var length = arr.map((item) => item.length);
-  var answer =
-    length.splice(length.indexOf(Math.max(...length)), 1)[0] *
-    length.splice(length.indexOf(Math.max(...length)), 1)[0];
-  return arr.length > 1 ? answer : 0;
+  })
+
+  const sort = answer.sort((a, b) => b - a)
+  return sort.length > 1 ? sort[0] * sort[1] : 0;
 }
